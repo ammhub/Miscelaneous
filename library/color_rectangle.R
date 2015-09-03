0a177b716c65e8c63fa79ee500245b0dd1415830
unknown <michaloa@NCI-01874798.nci.nih.gov>
2015-09-03 18:33:39 -0400

    remove ini

--- library/color_rectangle.R
+++
@@ -1,6 +0,0 @@
-# color rectangle area in a plot - graphics (stackoverflow)
-
-    color.rectangle <- function(color){ 
-        params <- par()$usr 
-        rect(params[1], params[3], params[2], params[4], col = color) 
-    } 
